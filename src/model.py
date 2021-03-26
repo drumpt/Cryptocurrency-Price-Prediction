07cdb7f36ef6c247808ddc755bcf29d4726f17d9
drumpt <ssohot1@kaist.ac.kr>
2021-03-26 10:10:49 +0900

    Before finish

--- src/model.py
+++ src/model.py
@@ -15,6 +15,7 @@ import preprocessor
 device = "cuda" if torch.cuda.is_available() else "cpu"
 if device != "cpu":
     torch.set_default_tensor_type("torch.cuda.FloatTensor")
+print(device)
 
 class CryptocurrencyPricePredictor():
     def __init__(self, mode, data_dir, output_dir, weight_dir, batch_size, epochs, look_back):
@@ -26,7 +27,7 @@ class CryptocurrencyPricePredictor():
         self.epochs = epochs
         self.look_back = look_back
 
-        self.model = LSTMPredictor(hidden_dim1 = 128, hidden_dim2 = 128)
+        self.model = LSTMPredictor(hidden_dim = 128, num_layers = 2)
         self.model.to(device)
         print(self.model)
 
@@ -34,18 +35,18 @@ class CryptocurrencyPricePredictor():
             self.model.load_state_dict(torch.load(self.weight_dir))
 
         if self.mode in ["train", "test"]:
-            self.dataset = CryptocurrencyDataset(self.data_dir)
+            self.dataset = CryptocurrencyDataset(self.data_dir, self.look_back)
 
         self.loss_function = nn.MSELoss()
-        self.optimizer = optim.Adam(params = self.model.parameters(), lr = 1e-5)
-        self.scheduler = optim.lr_scheduler.ExponentialLR(self.optimizer, gamma = 0.95)
+        self.optimizer = optim.Adam(params = self.model.parameters(), lr = 1e-4)
+        self.scheduler = optim.lr_scheduler.ExponentialLR(self.optimizer, gamma = 1)
 
     def train(self):
         train_size = int(0.8 * len(self.dataset))
         validation_size = len(self.dataset) - train_size
         train_set, validation_set = torch.utils.data.random_split(self.dataset, [train_size, validation_size])
-        train_dataloader = DataLoader(train_set, batch_size = self.batch_size, shuffle = True)
-        validation_dataloader = DataLoader(validation_set, batch_size = self.batch_size, shuffle = True)
+        train_dataloader = DataLoader(train_set, batch_size = self.batch_size, shuffle = False)
+        validation_dataloader = DataLoader(validation_set, batch_size = self.batch_size, shuffle = False)
 
         history = {"loss" : [], "val_loss" : []}
 
@@ -56,12 +57,12 @@ class CryptocurrencyPricePredictor():
 
             # train
             for item in tqdm(train_dataloader):
-                self.optimizer.zero_grad()
-
                 input_tensor, target_tensor = item["input_tensor"], item["output_tensor"]
                 output_tensor = self.model(input_tensor)
 
                 loss = self.loss_function(output_tensor, target_tensor)
+
+                self.optimizer.zero_grad()
                 loss.backward()
                 epoch_train_loss += loss
                 self.optimizer.step()
@@ -80,13 +81,13 @@ class CryptocurrencyPricePredictor():
             history["loss"].append(epoch_train_loss)
             history["val_loss"].append(epoch_validation_loss)
 
-            print(f"loss : {epoch_train_loss:.4f}, val_loss : {epoch_validation_loss:.4f}")
+            print(f"loss : {epoch_train_loss:.6f}, val_loss : {epoch_validation_loss:.6f}")
             self.save_model(epoch + 1, epoch_train_loss, epoch_validation_loss)
 
         self.plot(history)
 
     def test(self):
-        dataloader = DataLoader(self.dataset, batch_size = self.batch_size, shuffle = True)
+        dataloader = DataLoader(self.dataset, batch_size = self.batch_size, shuffle = False)
         test_loss = 0
 
         for item in tqdm(dataloader):
@@ -96,18 +97,16 @@ class CryptocurrencyPricePredictor():
             test_loss += self.loss_function(output_tensor, target_tensor)
 
         test_loss /= len(dataloader)
-        print(f"test_loss : {test_loss:.4f}")
+        print(f"test_loss : {test_loss:.6f}")
 
     def predict(self):
-        input_ratio_list, target_ratio_list = preprocessor.read_csv(self.data_dir, sort = "ratio", look_back = self.look_back)
-        input_price_list, target_price_list = preprocessor.read_csv(self.data_dir, sort = "price", look_back = self.look_back)
+        input_ratio_list, target_ratio_list = preprocessor.read_csv(self.data_dir, look_back = self.look_back, sort = "ratio")
+        input_price_list, target_price_list = preprocessor.read_csv(self.data_dir, look_back = self.look_back, sort = "price")
         output_ratio_list, output_price_list = [], []
 
         for i in range(len(input_ratio_list)):
-            output_ratio = self.model(torch.tensor(input_ratio_list[i], dtype = torch.float).unsqueeze(0).unsqueeze(-1)).detach()
-            output_ratio_list.append(output_ratio.squeeze(-1).squeeze(0))
-
-        print(output_ratio_list)
+            output_ratio = self.model(torch.tensor(input_ratio_list[i], dtype = torch.float).unsqueeze(0).unsqueeze(2)).detach()
+            output_ratio_list.append(output_ratio.squeeze(0).squeeze(0))
 
         output_price_list = target_price_list[0 : self.look_back]
         for i in range(len(output_ratio_list)):
@@ -115,12 +114,13 @@ class CryptocurrencyPricePredictor():
             output_price_list.append(output_price)
 
         plt.title("predicted price vs real price")
+        # plt.plot(target_ratio_list)
+        # plt.plot(output_ratio_list)
         plt.plot(target_price_list)
         plt.plot(output_price_list)
         plt.legend(['real price', 'predicted price'])
         plt.xlabel('day')
         plt.ylabel('price')
-        plt.show()
         plt.savefig(os.path.join(self.output_dir, "predict_result.png"))
 
     def save_model(self, epoch, loss, val_loss):
@@ -141,26 +141,31 @@ class CryptocurrencyPricePredictor():
         plt.savefig(os.path.join(self.output_dir, "training_result.png"))
 
 class LSTMPredictor(nn.Module):
-    def __init__(self, hidden_dim1, hidden_dim2):
+    def __init__(self, hidden_dim, num_layers):
         super(LSTMPredictor, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.dropout = nn.Dropout(0.25)
+        self.relu = nn.ReLU()
 
-        self.hidden_dim1 = hidden_dim1
-        self.hidden_dim2 = hidden_dim2
-
-        self.lstm1 = nn.LSTM(1, hidden_dim1)
-        self.lstm2 = nn.LSTM(hidden_dim1, hidden_dim2)
-        self.hidden2ratio = nn.Linear(hidden_dim2, 1)
+        self.lstm = nn.LSTM(1, hidden_dim, num_layers, batch_first = True)
+        self.fc1 = nn.Linear(hidden_dim, 32)
+        self.fc2 = nn.Linear(32, 1)
 
     def forward(self, x):
-        lstm1_out, _ = self.lstm1(x)
-        lstm2_out, _ = self.lstm2(lstm1_out)
-        ratio = self.hidden2ratio(lstm2_out[:, -1, :])
-        return ratio
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.dropout(self.relu(self.fc1(out[:, -1, :])))
+        out = self.fc2(out)
+        return out
 
 class CryptocurrencyDataset(Dataset):
-    def __init__(self, data_dir):
+    def __init__(self, data_dir, look_back):
         self.csv_files = preprocessor.get_all_csv_files(data_dir)
+        self.look_back = look_back
         self.input_list, self.output_list = [], []
+
         for csv_file in self.csv_files:
             input_list, output_list = preprocessor.read_csv(csv_file, sort = "ratio", look_back = self.look_back)
             self.input_list.extend(input_list)
--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,6 +1,8 @@
 import os
 import csv
 
+from sklearn.preprocessing import MinMaxScaler
+
 def get_all_csv_files(data_dir):
     csv_files = []
     for file in os.listdir(data_dir):
@@ -16,6 +18,7 @@ def read_csv(csv_dir, look_back, sort):
         if sort == "ratio":
             for row in reader: # row : [SNo, Name, Symbol, Date, High, Low, Open, Close, Volume, Marketcap]
                 data.append(float(row[7]) / float(row[6]) - 1)
+
             for i in range(len(data) - look_back - 1):
                 input_list.append(data[i : i + look_back])
                 output_list.append(data[i + look_back])
